d0663cec0bd0c686833d569a34e03ad99d47a6bf
Dan <dandundg@gmail.com>
2025-01-26 19:09:46 -0600

    Try different Sass compiler

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
-from sassutils.wsgi import SassMiddleware
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 10000
 
-app.wsgi_app = SassMiddleware(app.wsgi_app, {
-    'app': ('static/sass', 'static/css', '/static/css')
-})
+assets = Environment(app)
+# create bundle for Flask-Assets to compile and prefix scss to css
+css = Bundle('src/scss/styles.scss',
+             filters=['libsass'],
+             output='dist/css/styles.css',
+             depends='src/scss/*.scss')
+
+assets.register("asset_css", css)
+css.build()
 
 from app import routes
