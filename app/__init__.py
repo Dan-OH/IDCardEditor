60af30d360f70a2ae7268dbbf9577a12f0fac181
Dan-OH <daniel@ohmedia.ca>
2025-01-27 11:03:51 -0600

    Write cache to memory

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_assets import Environment, Bundle
+from webassets.cache import MemoryCache
 
 app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 10000
+
+app.config['ASSETS_CACHE'] = MemoryCache(capacity=1000)
 
 assets = Environment(app)
 # create bundle for Flask-Assets to compile and prefix scss to css
